46fd6702d459f634fc79a95170d00023c4c070b8
Matt Daley <m6daley@gmail.com>
2021-04-21 13:14:42 +0100

    Initial web app running jetty.

---
+++ src/test/java/com/sequsoft/jettydemo/JettydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sequsoft.jettydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JettydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
